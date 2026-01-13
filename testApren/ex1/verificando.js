03bce8398918900d091b93b097f176978893d4a3
stelsol <132797421+stelsol@users.noreply.github.com>
2026-01-12 21:32:00 -0300

    teste

---
+++ testApren/ex1/verificando.js
@@ -0,0 +1,12 @@
+let nome = "Stella"
+let idade = 15
+let m;
+
+if(idade < 18 ){
+        m = "Menor de idade "
+} else{
+        m = "maior de idade"
+}
+let resultado = m
+
+console.log(resultado)
