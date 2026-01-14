f1501c32f0df84bde80364f480e34943fcc22c58
stelsol <132797421+stelsol@users.noreply.github.com>
2026-01-13 22:38:46 -0300

    Exercio

--- testApren/ex1/verificando.js
+++ testApren/ex1/verificando.js
@@ -1,12 +1,68 @@
-let nome = "Stella"
-let idade = 15
-let m;
 
-if(idade < 18 ){
-        m = "Menor de idade "
-} else{
-        m = "maior de idade"
-}
-let resultado = m
 
-console.log(resultado)
+let form = document.querySelector('.formulario')
+
+form.addEventListener('submit', function(evento){
+        evento.preventDefault()
+        //console.log('bloqueado') // bloqueamos o envio do form para poder analizar melhoros dados a serem pegos]
+        let nome =  form.querySelector('#nome').value
+        let idade = Number(form.querySelector('#idade').value)
+
+        let verificando = idade < 18 ? "menor de idade" : "Maior de idade"
+
+        //console.log(`${nome} e ${verificando}`)
+        
+        let resultado = document.querySelector('#resultado')
+        let p = document.createElement('p')
+        p.innerHTML = ` Olá ${nome} você é ${verificando}`
+
+        resultado.innerHTML = ' '
+        resultado.appendChild(p)
+})
+
+
+
+
+
+
+// function verificandoIdade(){
+//         let nome =  form.getElementsByClassName('.nome')
+//         let idade = form.getElementsByClassName('.idade')
+
+
+
+// }
+// verificandoIdade()
+
+
+
+
+// function verificandoidade(){
+//         let nome = "Stella"
+//         let idade = 17
+//         let veridicado; 
+
+//         if(idade < 18 ){
+//                 veridicado = "Menor de idade"
+//         }else {
+//                 veridicado = "Maior de idade"
+//         }
+
+//         console.log(`Olá ${nome} você é ${veridicado}`) // ou podemos fazer um return para que afunction retorne algo para gente e asim exiba
+// }
+// verificandoidade()
+
+
+
+// let nome = "Stella"
+// let idade = 15
+// let m;
+
+// if(idade < 18 ){
+//         m = "Menor de idade "
+// } else{
+//         m = "maior de idade"
+// }
+// let resultado = m
+
+// console.log(resultado)
